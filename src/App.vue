8a9b0a6df140bc08b64322a406eea4f983e15ee3
Vivian Hayes <vivian.xw7@gmail.com>
2023-10-15 16:43:45 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Home from './components/Home.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Home msg="Look I changed the heading!!!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
